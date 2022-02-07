ceb87de78d58fb7f4a0db9fef9f7a80ed985e63d
mikolaj.kubista <mikolaj.kubista@gmail.com>
2022-02-07 16:28:15 +0100

    basci control flow

--- Battleships/Program.cs
+++ Battleships/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to BATTLESHIPS!");
+Console.WriteLine("Press 's' to start the game or 'x' to exit.");
+
+var loop = true;
+while(loop) {
+    var pressedKey = Console.ReadKey(true).KeyChar;
+    switch(pressedKey) {
+        case 's':
+            Console.WriteLine("Starting game...");
+            loop = false;
+            break;
+        case 'x':
+            Console.WriteLine("Exiting...");
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Invalid key pressed.");
+            break;
+    }
+}
